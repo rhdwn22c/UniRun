5312cfb29fa35ef5af5b23831bb457a964c978d0
rhdwn22c <rhdwn22c@gmail.com>
2022-08-05 14:40:13 +0900

    Updated Update Method

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -5,7 +5,10 @@ public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
     private void Update() {
-        //초당 speed의 속도로 왼쪽 평행이동
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        // 게임오버가 아니라면
+        if(!GameManager.instance.isGameover) {
+            // 초당 speed의 속도로 왼쪽 평행이동
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
