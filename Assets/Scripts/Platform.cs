1916acb198d5ff2541d435a7b6c9d7b347e6c907
rhdwn22c <rhdwn22c@gmail.com>
2022-08-07 17:01:55 +0900

    Made OnCollisionEnter2D Method

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -23,6 +23,11 @@ public class Platform : MonoBehaviour {
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
-        // 플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
+        // 충돌한 상대방의 태그가 Player이고, 전에 플레이어가 밟지 않았다면
+        if(collision.collider.tag == "Player" && !stepped) {
+            // 점수를 추가하고 밟힘 상태를 참으로 변경
+            stepped = true;
+            GameManager.instance.AddScore(1);
+        }
     }
 }
